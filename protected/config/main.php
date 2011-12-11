ef94213c66526b632eb6971be6b93a8e45652b67
unknown <Oscar Sambrano@Andisi-Oscar.(none)>
2011-12-11 11:01:35 -0500

    first commit

--- protected/config/main.php
+++ protected/config/main.php
@@ -20,14 +20,14 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'Prueba',
 		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+		
 	),
 
 	// application components
@@ -47,26 +47,27 @@ return array(
 			),
 		),
 		
-		/*'db'=>array(
+		'db1'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
 		
+//		'db2'=>array(
+//			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+//			'emulatePrepare' => true,
+//			'username' => 'root',
+//			'password' => '',
+//			'charset' => 'utf8',
+//		),
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
-		'db'=>array(
-			'connectionString' => 'pgsql:host=localhost;port=5432;dbname=freerisk_erpscan',
+			'connectionString' => 'pgsql:host=localhost;port=5432;dbname=erpScan',
 			'emulatePrepare' => true,
 			'username' => 'postgres',
-			'password' => 'postgres',
+			'password' => 'root',
 			'charset' => 'utf8',
 		),
+            
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
             'errorAction'=>'site/error',
